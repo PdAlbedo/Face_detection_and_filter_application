21194c453dadc062a3aa76095e76d555d7ef842a
xichen <xic.liu.0815@gmail.com>
2022-04-28 11:24:25 -0500

    try to modify

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 # This is a sample Python script.
+from time import time
 
 import cv2
 import numpy
 import dlib
+import numpy as np
+
+WINDOWS_WID = 480
+WINDOWS_HEI = 360
 
 SCALE_FACTOR = 1
 FEATHER_AMOUNT = 11
@@ -16,67 +21,74 @@ LEFT_EYE_POINTS = list(range(42, 48))
 NOSE_POINTS = list(range(27, 35))
 JAW_POINTS = list(range(0, 17))
 
-COLOUR_CORRECT_BLUR_FRAC = 0.6
+COLOUR_CORRECT_BLUR_FRAC = 0.5
 
 # Points from the second image to overlay on the first. The convex hull of each
 # element will be overlaid.
-OVERLAY_POINTS = [
-    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
-    NOSE_POINTS + MOUTH_POINTS,
-]
+# OVERLAY_POINTS = [
+#     LEFT_EYE_POINTS + RIGHT_EYE_POINTS,
+#     NOSE_POINTS + MOUTH_POINTS,
+# ]
+OVERLAY_POINTS = [list(range(0, 68))]
 
 # Points used to line up the images.
 ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
                 RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
 
 
-def draw_convex_hull(im, points, color):
+def draw_convex_hull(img, points, color):
     points = cv2.convexHull(points)
-    cv2.fillConvexPoly(im, points, color=color)
+    cv2.fillConvexPoly(img, points, color = color)
 
 
-def correct_colours(im1, im2, landmarks1):
+def correct_colours(img1, img2, landmarks1):
     blur_amount = COLOUR_CORRECT_BLUR_FRAC * numpy.linalg.norm(
-        numpy.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
-        numpy.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
+        numpy.mean(landmarks1[LEFT_EYE_POINTS], axis = 0) -
+        numpy.mean(landmarks1[RIGHT_EYE_POINTS], axis = 0))
     blur_amount = int(blur_amount)
     if blur_amount % 2 == 0:
         blur_amount += 1
-    im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
-    im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
+    img1_blur = cv2.GaussianBlur(img1, (blur_amount, blur_amount), 0)
+    img2_blur = cv2.GaussianBlur(img2, (blur_amount, blur_amount), 0)
 
     # Avoid divide-by-zero errors.
-    im2_blur += (128 * (im2_blur <= 1.0)).astype(im2_blur.dtype)
+    img2_blur += (128 * (img2_blur <= 1.0)).astype(img2_blur.dtype)
 
-    return (im2.astype(numpy.float64) * im1_blur.astype(numpy.float64) /
-            im2_blur.astype(numpy.float64))
+    return (img2.astype(numpy.float64) * img1_blur.astype(numpy.float64) /
+            img2_blur.astype(numpy.float64))
 
 
-def warp_im(im, M, dshape):
-    output_im = numpy.zeros(dshape, dtype=im.dtype)
+def warp_img(im, M, dshape):
+    output_img = numpy.zeros(dshape, dtype = im.dtype)
     cv2.warpAffine(im,
                    M[:2],
                    (dshape[1], dshape[0]),
-                   dst=output_im,
-                   borderMode=cv2.BORDER_TRANSPARENT,
-                   flags=cv2.WARP_INVERSE_MAP)
-    return output_im
+                   dst = output_img,
+                   borderMode = cv2.BORDER_TRANSPARENT,
+                   flags = cv2.WARP_INVERSE_MAP)
+    return output_img
 
 
-def get_face_mask(im, landmarks):
-    im = numpy.zeros(im.shape[:2], dtype=numpy.float64)
+def get_face_mask(img, landmarks):
+    # im = numpy.zeros(img.shape[:2], dtype = numpy.float64)
+    # print(im.shape)
+    #
+    # for group in OVERLAY_POINTS:
+    #     draw_convex_hull(im,
+    #                      landmarks[group],
+    #                      color = 1)
+    #
+    # img = numpy.array([im, im, im]).transpose((1, 2, 0))
+    # cv2.imshow('tmp', img)
+    # cv2.waitKey()
+    mask = np.zeros_like(img)
+    contex_hull = cv2.convexHull(landmarks)
+    cv2.fillConvexPoly(mask, contex_hull, (255, 255, 255))
 
-    for group in OVERLAY_POINTS:
-        draw_convex_hull(im,
-                         landmarks[group],
-                         color=1)
+    # img = (cv2.GaussianBlur(img, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    # img = cv2.GaussianBlur(img, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
 
-    im = numpy.array([im, im, im]).transpose((1, 2, 0))
-
-    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
-    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
-
-    return im
+    return mask
 
 
 def transformation_from_points(points1, points2):
@@ -86,15 +98,12 @@ def transformation_from_points(points1, points2):
     is minimized.
     """
     # Solve the procrustes problem by subtracting centroids, scaling by the
-    # standard deviation, and then using the SVD to calculate the rotation. See
-    # the following for more details:
-    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
-
+    # standard deviation, and then using the SVD to calculate the rotation.
     points1 = points1.astype(numpy.float64)
     points2 = points2.astype(numpy.float64)
 
-    c1 = numpy.mean(points1, axis=0)
-    c2 = numpy.mean(points2, axis=0)
+    c1 = numpy.mean(points1, axis = 0)
+    c2 = numpy.mean(points2, axis = 0)
     points1 -= c1
     points2 -= c2
 
@@ -117,103 +126,113 @@ def transformation_from_points(points1, points2):
 
 
 def main():
-    if_save = False
-    if_swich = False
+    is_shown_feats = False
+    is_face_exchange = False
 
     detector = dlib.get_frontal_face_detector()
     # Load the predictor
     predictor = dlib.shape_predictor("../data/shape_predictor_68_face_landmarks.dat")
     # read the image
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, WINDOWS_WID)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, WINDOWS_HEI)
     while True:
-        ret, frame = cap.read()
-        demo = cap.read()
+        # ret, frame = cap.read()
+        # exchanged_faces = cap.read()
+        frame = cv2.imread('tmp.png')
+        frame = cv2.resize(frame, (int(frame.shape[1] * 20 / 100), int(frame.shape[0] * 20 / 100)))
+        exchanged_faces = frame.copy()
         # Convert image into grayscale
-        gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(src = frame, code = cv2.COLOR_BGR2GRAY)
         # Use detector to find landmarks
         faces = detector(gray)
 
-        if if_save:
+        if is_shown_feats:
             print(len(faces))
             for face in faces:
-                x1 = face.left()  # left point
-                y1 = face.top()  # top point
-                x2 = face.right()  # right point
-                y2 = face.bottom()  # bottom point
                 # Create landmark object
-                landmarks = predictor(image=gray, box=face)
-
+                landmarks = predictor(image = gray, box = face)
                 # Loop through all the points
+                c = 0
                 for n in range(0, 68):
                     x = landmarks.part(n).x
                     y = landmarks.part(n).y
-                    # Draw a circle
-                    cv2.circle(img=frame, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
-        if if_swich:
+                    cv2.putText(img = frame, text = str(c), org = (x, y), fontFace = cv2.FONT_HERSHEY_SIMPLEX,
+                                fontScale = 0.3, color = (0, 0, 255))
+                    c += 1
+        if is_face_exchange:
             if len(faces) < 2:
-                # if_swich = False
-                demo = frame
-                print("not enough faces")
+                exchanged_faces = frame
+                print("Not enough faces")
             else:
+                # TODO: May change to exchange multiple faces randomly
                 landmarks1 = numpy.matrix([[p.x, p.y] for p in predictor(frame, faces[0]).parts()])
                 landmarks2 = numpy.matrix([[p.x, p.y] for p in predictor(frame, faces[1]).parts()])
 
-                M1 = transformation_from_points(landmarks1[ALIGN_POINTS],
-                                                landmarks2[ALIGN_POINTS])
+                M1 = transformation_from_points(landmarks1, landmarks2)
+                M2 = transformation_from_points(landmarks2, landmarks1)
 
-                M2 = transformation_from_points(landmarks2[ALIGN_POINTS],
-                                                landmarks1[ALIGN_POINTS])
-
-                mask1 = get_face_mask(frame, landmarks2)
+                mask1 = get_face_mask(gray, landmarks2)
                 mask2 = get_face_mask(frame, landmarks1)
-                warped_mask1 = warp_im(mask1, M1, frame.shape)
-                warped_mask2 = warp_im(mask2, M2, frame.shape)
-                combined_mask1 = numpy.max([get_face_mask(frame, landmarks1), warped_mask1],
-                                           axis=0)
-                combined_mask2 = numpy.max([get_face_mask(frame, landmarks2), warped_mask2],
-                                           axis=0)
-                warped_im1 = warp_im(frame, M1, frame.shape)
-                warped_im2 = warp_im(frame, M2, frame.shape)
-                warped_corrected_im1 = correct_colours(frame, warped_im1, landmarks1)
-                warped_corrected_im2 = correct_colours(frame, warped_im2, landmarks2)
+                print(mask1.shape)
+                cv2.imshow('mask1', mask1)
+                cv2.imshow('mask2', mask2)
 
-                output_im = frame * (1.0 - combined_mask1) + warped_corrected_im1 * combined_mask1
-                # output_im = frame * (1.0  - combined_mask1)*(1.0- combined_mask2) + warped_corrected_im2 * combined_mask2 + warped_corrected_im1 * combined_mask1
-                output_im = output_im * (1.0 - combined_mask2) + warped_corrected_im2 * combined_mask2
+                warped_mask1 = warp_img(mask1, M1, frame.shape)
+                warped_mask2 = warp_img(mask2, M2, frame.shape)
+                cv2.imshow('warped_mask1', warped_mask1)
+                cv2.imshow('warped_mask2', warped_mask2)
 
-                #cv2.imwrite(frame, output_im)
+                combined_mask1 = numpy.max([get_face_mask(frame, landmarks1), warped_mask1], axis = 0)
+                combined_mask2 = numpy.max([get_face_mask(frame, landmarks2), warped_mask2], axis = 0)
+                cv2.imshow('combined_mask1', combined_mask1)
+                cv2.imshow('combined_mask2', combined_mask2)
 
-                demo = output_im.astype(numpy.uint8)
-                cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
-                # cv2.imshow('Display', demo)
+                warped_img1 = warp_img(frame, M1, frame.shape)
+                warped_img2 = warp_img(frame, M2, frame.shape)
+                cv2.imshow('warped_img1', warped_img1)
+                cv2.imshow('warped_img2', warped_img2)
 
-                print("sucess")
-                # if_swich = False
+                # warped_corrected_img1 = correct_colours(frame, warped_img1, landmarks1)
+                # warped_corrected_img2 = correct_colours(frame, warped_img2, landmarks2)
+
+                # output_img = frame * (1.0 - combined_mask1) + warped_corrected_img1 * combined_mask1
+                output_img = frame * (1.0 - combined_mask1) + warped_img1 * combined_mask1
+                output_img = cv2.bitwise_not(frame, frame, mask = mask1)
+                # output_img = output_img * (1.0 - combined_mask2) + warped_corrected_img2 * combined_mask2
+
+                exchanged_faces = output_img.astype(numpy.uint8)
+                # cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
+                # print("success")
         else:
-            demo = frame
-        cv2.imshow('Display', demo)
-        cv2.imshow(winname="Face", mat=frame)
+            exchanged_faces = frame
+
+        # cv2.namedWindow('Display')
+        # cv2.resizeWindow('Display', WINDOWS_WID, WINDOWS_HEI)
+        cv2.imshow('Display', exchanged_faces)
+        # cv2.namedWindow('Face')
+        # cv2.resizeWindow('Face', WINDOWS_WID, WINDOWS_HEI)
+        cv2.imshow('Face', frame)
+
         # Close all windows
         k = cv2.waitKey(10)
-        if k == 113:
+        if k == ord('q'):
             cv2.destroyAllWindows()
             break
-        if k == 115:
+        if k == ord('s'):
             print("pressed s")
-            if if_save == True:
-                if_save = False
+            if is_shown_feats:
+                is_shown_feats = False
             else:
-                if_save = True
-        if k == 119:
+                is_shown_feats = True
+        if k == ord('w'):
             print("pressed w")
-            if if_swich == True:
-                if_swich = False
+            if is_face_exchange:
+                is_face_exchange = False
             else:
-                if_swich = True
+                is_face_exchange = True
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
