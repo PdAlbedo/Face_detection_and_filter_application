68ef8615b694a49c1f05d041f80e396fc8443b7c
Sida Zhang <sidazhang.97@gmail.com>
2022-05-03 23:34:35 -0400

    Update livevideo_gui.py

--- src/livevideo_gui.py
+++ src/livevideo_gui.py
@@ -1,4 +1,9 @@
 """
+CS5330 - Pattern Recognition & Computer Vision
+Project Title: Face detection and filter application
+April 18, 2022
+Team: Sida Zhang, Xichen Liu, Xiang Wang, Hongyu Wan
+
 Description: ain function of application
 """
 __author__ = "Sida Zhang, Hongyu Wan, Xiang Wang, Xichen Liu"
